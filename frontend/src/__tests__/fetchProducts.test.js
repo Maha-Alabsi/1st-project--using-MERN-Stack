cd5c094f5347f30dbaff121022f29ae015969fea
Maha Alabsi <malabsi@aspire-services.org>
2021-05-09 18:20:25 +0300

    edit

--- frontend/src/__tests__/fetchProducts.test.js
+++ frontend/src/__tests__/fetchProducts.test.js
@@ -18,7 +18,6 @@ const mockProduct = {
   updatedAt: '2021-04-14T13:20:46.189Z',
 };
 
-
 test('Mocking Fetch', async () => {
     try {
     fetch.mockResponse(JSON.stringify(mockProduct));
@@ -29,7 +28,6 @@ test('Mocking Fetch', async () => {
     console.log(error.message);
   }
 });
-
 
 
 // global.fetch = jest.fn(() => {
@@ -53,16 +51,3 @@ test('Mocking Fetch', async () => {
 // });
 
 
-// {
-//     "_id": "6076ec2ebc2d7b4d505449b3",
-//     "name": "Table",
-//     "image": "/imgs/img21.jpg",
-//     "price": "45$",
-//     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dolor orci, euismod at.",
-//     "size": "30*h x 20*w x 22.75*d (76.5x51x58cm)",
-//     "material": "wood/plastic composition with 25% wood fibers +70% PP (polypoplen) +5% coloured PP",
-//     "status": "New",
-//     "__v": 0,
-//     "createdAt": "2021-04-14T13:20:46.189Z",
-//     "updatedAt": "2021-04-14T13:20:46.189Z"
-//   },
